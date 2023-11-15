6f181bd0020a443e341692caa703cbbd552c2c95
Timotheus Wiese <80679542+dabuhuhu@users.noreply.github.com>
2023-11-15 16:20:43 +0100

    update script

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
 const mushroom = document.querySelector('.mushroom');
-let growTimeout;
-let clickCount = 0;
+let secondMushroomAdded = false;
 
-mushroom.addEventListener('click', function() {
-    clearTimeout(growTimeout);
-    clickCount++;
+function getRandomPosition(element) {
+    const maxX = window.innerWidth - element.offsetWidth;
+    const maxY = window.innerHeight - element.offsetHeight;
+    return { x: Math.random() * maxX, y: Math.random() * maxY };
+}
 
-    if (clickCount >= 3) {
-        this.style.transform = 'scale(1)';
-        clickCount = 0;
+function moveElement(element) {
+    const { x, y } = getRandomPosition(element);
+    element.style.left = x + 'px';
+    element.style.top = y + 'px';
+}
+
+function resizeElement(element) {
+    const scale = Math.random() > 0.5 ? 1.2 : 0.8;
+    element.style.transform = `scale(${scale})`;
+}
+
+function addSecondMushroom() {
+    if (secondMushroomAdded) return;
+
+    const newMushroom = mushroom.cloneNode(true);
+    document.body.appendChild(newMushroom);
+    moveElement(newMushroom);
+    secondMushroomAdded = true;
+}
+
+mushroom.addEventListener('mouseenter', function() {
+    if (Math.random() > 0.7) {
+        moveElement(this);
     } else {
-        this.style.transform = 'scale(1.2)';
+        resizeElement(this);
     }
 
-    growTimeout = setTimeout(() => {
-        this.style.transform = 'scale(1)';
-    }, 3000);
+    if (Math.random() > 0.9) {
+        addSecondMushroom();
+    }
+});
+
+mushroom.addEventListener('click', function() {
+    this.style.transform = 'scale(1)';
+    this.style.left = '50%';
+    this.style.top = '50%';
+    this.style.transform = 'translate(-50%, -50%)';
+});
+
+// Apply the same logic to the second mushroom if it exists
+document.body.addEventListener('click', function(e) {
+    if (e.target !== mushroom && e.target.className === 'mushroom') {
+        e.target.style.transform = 'scale(1)';
+        e.target.style.left = '50%';
+        e.target.style.top = '50%';
+        e.target.style.transform = 'translate(-50%, -50%)';
+    }
 });
